1661bdc83948138fdfecea94782eb557fa9ea098
himitsu <firman@sagita.tech>
2020-05-01 10:19:31 +0700

    little improvement

--- src/Components/Layouts/FoodNutri.js
+++ src/Components/Layouts/FoodNutri.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import ImageIcon from "@material-ui/icons/Image";
+import ButtonBase from "@material-ui/core/ButtonBase";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,28 +14,66 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        margin: '18px 5%',
+        maxWidth: 1280,
     },
+    image: {
+        width: 75,
+        height: 75,
+        align: 'center',
+    },
+    img: {
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+    }
 }));
 
 export default function FoodNutri() {
     const classes = useStyles();
+    const items = [{
+        name: "Random Name #1",
+        calories: "XXX",
+        nutrients : "XXX"
+    },{
+        name: "Random Name #2",
+        calories: "XXX",
+        nutrients : "XXX"
+    },{
+        name: "Random Name #2",
+        calories: "XXX",
+        nutrients : "XXX"
+    },{
+        name: "Random Name #2",
+        calories: "XXX",
+        nutrients : "XXX"
+    }];
+
 
     return (
         <div className={classes.root}>
-            <Paper>
+            <Paper className={classes.paper}>
                 <Grid container spacing={3}>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
+                    {items.map((item) =>
+                        <Grid item xs={3}>
+                            <Paper className={classes.paper}>
+                                <Typography gutterBottom variant="subtitle1">
+                                    {item.name}
+                                </Typography>
+                                <ButtonBase className={classes.image}>
+                                    <ImageIcon className={classes.img}/>
+                                </ButtonBase>
+                                <Typography variant="body2" color="textSecondary">
+                                    {item.calories}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    {item.nutrients}
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                    )}
+
                 </Grid>
             </Paper>
         </div>
